4858c2f38761ce95e119d106db7b99733b173ef9
KeitoIsurugi <ja224kg@gmail.com>
2021-09-03 20:12:22 +0900

    add comment

--- auto_typinggame.rb
+++ auto_typinggame.rb
@@ -2,13 +2,17 @@ require 'selenium-webdriver'
 
 d = Selenium::WebDriver.for :chrome
 
+# タイピングゲームページにアクセスし、スペースキーでゲームスタート
 d.get("https://typing.twi1.me/game/18144")
-d.find_element(:class, 'mtjSeSc-settingBtn').click
+sleep(2)
+d.find_element(:css, 'body').send_keys(:space)
 sleep(2)
 d.find_element(:css, 'body').send_keys(:space)
 sleep(2)
 d.find_element(:css, 'body').send_keys(:space)
 
+# タイピングする文字列を取得
+# 取得した文字列をタイピング
 loop do
   text = d.find_element(:css, '.mtjGmSc-roma .mtjNowInput').text
   d.find_element(:css, 'body').send_keys(text)
